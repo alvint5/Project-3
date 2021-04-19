762e625ccb7a865bd3122ee20e24caa5fa17fbc6
Alvin Thomas <alvinthomas@ufl.edu>
2021-04-18 22:30:27 -0400

    Possible Final Version?

--- main.cpp
+++ main.cpp
@@ -80,8 +80,8 @@ void getCVSData(string filePath, unordered_multimap<string, Game>& gameMap, unor
 
 			//push onto data structures.
 			Game gameProperties(title.substr(1, title.size() - 2), year, month, avg, gain, peak, avgpeak.substr(1, avgpeak.size() - 2));
-			gameMap.emplace(title.substr(1, title.size() - 2), gameProperties);
-			gameNames.insert(title.substr(1, title.size() - 2));
+			gameMap.emplace(title.substr(1, title.size() - 2), gameProperties); //insert game object into hash table with name as key
+			gameNames.insert(title.substr(1, title.size() - 2)); //keep track of which game titles are in table with set
 		}
 	}
 	else {
@@ -91,10 +91,10 @@ void getCVSData(string filePath, unordered_multimap<string, Game>& gameMap, unor
 
 
 vector<Game> getBucketData(unordered_multimap<string, Game>& myMap, string _name) {
-	int i = myMap.bucket(_name);
+	int i = myMap.bucket(_name); //finds out which bucket of hash table game objects are in 
 	vector<Game> temp;
 	for (auto iter = myMap.begin(i); iter != myMap.end(i); ++iter) {
-		temp.push_back(iter->second);
+		temp.push_back(iter->second); //creates a vector of all elements in that bucket of hash table
 	}
 	return temp;
 }
@@ -103,20 +103,20 @@ Game jumpSearch(unordered_multimap<string, Game>& myMap, string _name, int _mont
 	vector<Game> games = getBucketData(myMap, _name);
 	int m = sqrt(games.size()); //optimal value obtained from geeks for geeks explanation of jump search
 
-	for (int i = 0; i < games.size(); i += m) {
+	for (int i = 0; i < games.size(); i += m) { //index incremented by m rather than 1
 		if (games[i].getYear() <= _year) {
 			if (games[i].getMonth() < _month) {
-				i -= m;
-				while (i < i + m) {
+				i -= m; //went too far in vector so go back a step
+				while (i < i + m) { //linear search until element is found
 					if (games[i].getYear() == _year && games[i].getMonth() == _month) {
 						return games[i];
 					}
 					i++;
 				}
-				//cout << "not in bucket" << endl;
+				//if gets outside of while loop, it's not in table
 			}
-			else {
-				while (i < i + 12) {
+			else { //year is correct but month is greater than target
+				while (i < i + 12) { //12 months in a year
 					if (i < games.size()) {
 						if (games[i].getYear() == _year && games[i].getMonth() == _month) {
 							return games[i];
@@ -124,16 +124,15 @@ Game jumpSearch(unordered_multimap<string, Game>& myMap, string _name, int _mont
 						i++;
 					}
 					else {
-						//cout << "not in bucket" << endl;
-						Game temp(2012);
-						return temp;
+						//i has grown too large and is no longer in vector range, data not in table
+						Game temp(2012); //sets year to -1 for future checks
+						return temp; 
 					}
 				}
-				//cout << "not in bucket" << endl;
 			}
 		}
 	}
-	Game temp(2012);
+	Game temp(2012); //sets year to -1
 	return temp;
 }
 
@@ -239,22 +238,22 @@ Game FibonacciSearch(unordered_multimap<string, Game>& myMap, string _name, int 
 
 
 struct myComparator {
-	int operator() (Game& _game1, Game& _game2) {
+	int operator() (Game& _game1, Game& _game2) { //max heap comparator using Avg players as comparison
 		return _game1.getAvg() < _game2.getAvg();
 	}
 };
 
 	
-vector<Game> top10GamesJump(unordered_multimap<string, Game>& myMap, int _month, int _year) {
+vector<Game> top10GamesJump(unordered_multimap<string, Game>& myMap, int _month, int _year) { //determines top 10 most popular games in specified month and year
 	priority_queue<Game, vector<Game>, myComparator> maxHeap;
 	vector<Game> games;
 
-	for (int i = 0; i < myMap.bucket_count(); ++i) {
+	for (int i = 0; i < myMap.bucket_count(); ++i) { //checks all the buckets
 		auto iter = myMap.begin(i);
-		if (iter != myMap.end(i)) {
+		if (iter != myMap.end(i)) { //bucket is not empty
 			Game temp = jumpSearch(myMap, iter->first, _month, _year);
-			if (temp.getYear() != -1) {
-				maxHeap.push(temp);
+			if (temp.getYear() != -1) { //false game object created during jumpsearch
+				maxHeap.push(temp); //actual game object with specified month and year is pushed into max heap
 			}
 		}
 	}
@@ -262,8 +261,8 @@ vector<Game> top10GamesJump(unordered_multimap<string, Game>& myMap, int _month,
 	int temp = 10;
 
 	while (temp > 0) {
-		Game temp2 = maxHeap.top();
-		games.push_back(temp2);
+		Game temp2 = maxHeap.top(); 
+		games.push_back(temp2); //push 10 most popular games into vector and return it
 		maxHeap.pop();
 		temp--;
 	}
@@ -271,7 +270,7 @@ vector<Game> top10GamesJump(unordered_multimap<string, Game>& myMap, int _month,
 	return games;
 }
 
-vector<Game> top10GamesFib(unordered_multimap<string, Game>& myMap, int _month, int _year) {
+vector<Game> top10GamesFib(unordered_multimap<string, Game>& myMap, int _month, int _year) { //same function as above, but using fibonacci search instead of jump search to compare them 
 	priority_queue<Game, vector<Game>, myComparator> maxHeap;
 	vector<Game> games;
 
@@ -297,7 +296,7 @@ vector<Game> top10GamesFib(unordered_multimap<string, Game>& myMap, int _month, 
 	return games;
 }
 
-vector<Game> top10AllTime(unordered_multimap<string, Game>& myMap) {
+vector<Game> top10AllTime(unordered_multimap<string, Game>& myMap) { //iterates through entire table to find greatest player bases at any given time
 	priority_queue<Game, vector<Game>, myComparator> maxHeap;
 	vector<Game> games;
 
@@ -319,10 +318,10 @@ vector<Game> top10AllTime(unordered_multimap<string, Game>& myMap) {
 	return games;
 }
 
-vector<Game> top10AllTimeNoDupes(unordered_multimap<string, Game>& myMap) {
+vector<Game> top10AllTimeNoDupes(unordered_multimap<string, Game>& myMap) { //same function as above but doesn't allow same game to occur again in leaderboard
 	priority_queue<Game, vector<Game>, myComparator> maxHeap;
 	vector<Game> games;
-	unordered_set<string> names;
+	unordered_set<string> names; //keeps track of which games are already in the leaderboard
 
 	for (auto iter = myMap.begin(); iter != myMap.end(); ++iter) {
 		if (iter->second.getYear() != -1) {
@@ -334,13 +333,13 @@ vector<Game> top10AllTimeNoDupes(unordered_multimap<string, Game>& myMap) {
 
 	while (temp > 0) {
 		Game temp2 = maxHeap.top();
-		if (names.find(temp2.getTitle()) == names.end()) {
+		if (names.find(temp2.getTitle()) == names.end()) { //that game is not in leaderboard
 			games.push_back(temp2);
 			maxHeap.pop();
 			temp--;
 			names.insert(temp2.getTitle());
 		}
-		else {
+		else { //already in leaderboard so not pushed into vector again
 			maxHeap.pop();
 		}
 	}
@@ -348,7 +347,7 @@ vector<Game> top10AllTimeNoDupes(unordered_multimap<string, Game>& myMap) {
 	return games;
 }
 
-bool validYearInput(int _year) {
+bool validYearInput(int _year) { //ensures year inputs are between 2012 and 2021
 	if (_year > 2021) {
 		cout << "Latest data is from 2021.\n" << endl;
 		return false;
@@ -365,7 +364,7 @@ bool validYearInput(int _year) {
 bool validInput(int _month, int _year) {
 	if (validYearInput(_year) == true) {
 		if (_year == 2012) {
-			if (_month > 6 && _month < 13) {
+			if (_month > 6 && _month < 13) { //earliest data in table is from 7/2012
 				return true;
 			}
 			else {
@@ -374,7 +373,7 @@ bool validInput(int _month, int _year) {
 			}
 		}
 		else if (_year == 2021) {
-			if (_month > 0 && _month < 3) {
+			if (_month > 0 && _month < 3) { //latest data in table is from 2/2021
 				return true;
 			}
 			else {
@@ -385,8 +384,8 @@ bool validInput(int _month, int _year) {
 			if (_month > 0 && _month < 13) {
 				return true;
 			}
-			else {
-				cout << "Month should be an integer between 1 and 12.\n" << endl;
+			else { //incorrect month input
+				cout << "Month should be an integer between 1 and 12.\n" << endl; 
 				return false;
 			}
 		}
@@ -394,7 +393,7 @@ bool validInput(int _month, int _year) {
 }
 
 bool validGameTitle(string name, unordered_set<string>& names) {
-	if (names.find(name) != names.end()) {
+	if (names.find(name) != names.end()) { //checks to see inputted game title is in set of names created during initialization
 		return true;
 	}
 	else {
@@ -414,8 +413,8 @@ int main()
 	getCVSData("data/steamcharts.csv", gameMap, names);
 
 	int choice = 1;
-	while (choice != 7) {
-		cout << "1. Game Information\n";
+	while (choice != 7) { //end program if user inputs 7 during menu
+		cout << "1. Game Information\n"; //choices for user to choose from
 		cout << "2. Highest Average Playerbase\n";
 		cout << "3. Monthly Gain in Playerbase\n";
 		cout << "4. Monthly Peak in Playerbase\n";
@@ -429,25 +428,25 @@ int main()
 			string temp;
 			cout << "Insert game title: " << endl;
 			getline(cin, temp);
-			if (validGameTitle(temp, names) == true) {
+			if (validGameTitle(temp, names) == true) { //ensure game is in table
 				cout << "Insert year: " << endl;
 				int temp2;
 				cin >> temp2;
 				cout << "Insert month: " << endl;
 				int temp3;
 				cin >> temp3;
-				if (validInput(temp3, temp2) == true) {
-					auto start = chrono::steady_clock::now();
+				if (validInput(temp3, temp2) == true) { //ensure month and year inputs make sense
+					auto start = chrono::steady_clock::now(); //notes the time before jump search begins
 					Game temp4 = jumpSearch(gameMap, temp, temp3, temp2);
-					auto end = chrono::steady_clock::now();
-					chrono::duration<double> jump_time = end - start;
+					auto end = chrono::steady_clock::now(); //notes time after jump search ends
+					chrono::duration<double> jump_time = end - start; //the difference is what is output to the user
 
 					start = chrono::steady_clock::now();
 					Game temp5 = FibonacciSearch(gameMap, temp, temp3, temp2);
 					end = chrono::steady_clock::now();
 					chrono::duration<double> fib_time = end - start;
 					if (temp4.getYear() == -1) {
-						cout << "Game data not found for this date." << endl;
+						cout << "Game data not found for this date." << endl; //game came out later than that date
 					}
 					else {
 						cout << temp4.getTitle() << endl;
@@ -455,13 +454,13 @@ int main()
 						cout << "Peak Player Count: " << temp4.getPeak() << endl;
 						cout << "Gain from Previous Month: " << temp4.getGain() << endl;
 						cout << "Average/Peak Percentage: " << temp4.getAvgPeak() << endl;
-						cout << "\nJump Search Algorithm Time: " << jump_time.count() << " s" << endl;
+						cout << "\nJump Search Algorithm Time: " << jump_time.count() << " s" << endl; //outputs both search times for comparison
 						cout << "Fibonacci Search Algorithm Time: " << fib_time.count() << " s\n" << endl;
 					}
 				}
 			}
 		}
-		else if (choice == 2) {
+		else if (choice == 2) { //top 10 games in user inputted year and month
 			int temp;
 			int temp2;
 			cout << "Insert year: " << endl;
@@ -485,7 +484,7 @@ int main()
 				cout << "Fibonacci Search Algorithm Time: " << fib_time.count() << " s\n" << endl;
 			}
 		}
-		else if (choice == 3) {
+		else if (choice == 3) { //12 months of gain for specified year
 			cout << "Insert game title:" << endl;
 			string temp3;
 			getline(cin, temp3);
@@ -497,12 +496,17 @@ int main()
 				if (validYearInput(temp) == true) {
 					auto start = chrono::steady_clock::now();
 					for (int i = 1; i < 13; i++) {
-						Game temp2 = jumpSearch(gameMap, temp3, i, temp);
-						if (temp2.getYear() == -1) {
+						if (temp == 2021 && i > 2) {
 							cout << i << ": No Info For This Month" << endl;
 						}
 						else {
-							cout << i << ": " << temp2.getGain() << endl;
+							Game temp2 = jumpSearch(gameMap, temp3, i, temp);
+							if (temp2.getYear() == -1) {
+								cout << i << ": No Info For This Month" << endl;
+							}
+							else {
+								cout << i << ": " << temp2.getGain() << endl;
+							}
 						}
 					}
 					auto end = chrono::steady_clock::now();
@@ -526,7 +530,7 @@ int main()
 				}
 			}
 		}
-		else if (choice == 4) {
+		else if (choice == 4) { // 12 months of peak for specified year
 			cout << "Insert game title:" << endl;
 			string temp3;
 			getline(cin, temp3);
@@ -538,12 +542,17 @@ int main()
 				if (validYearInput(temp) == true) {
 					auto start = chrono::steady_clock::now();
 					for (int i = 1; i < 13; i++) {
-						Game temp2 = jumpSearch(gameMap, temp3, i, temp);
-						if (temp2.getYear() == -1) {
+						if (temp == 2021 && i > 2) {
 							cout << i << ": No Info For This Month" << endl;
 						}
 						else {
-							cout << i << ": " << temp2.getPeak() << endl;
+							Game temp2 = jumpSearch(gameMap, temp3, i, temp);
+							if (temp2.getYear() == -1) {
+								cout << i << ": No Info For This Month" << endl;
+							}
+							else {
+								cout << i << ": " << temp2.getPeak() << endl; //same as 3 but output peak rather than gain
+							}
 						}
 					}
 					auto end = chrono::steady_clock::now();
@@ -567,7 +576,7 @@ int main()
 				}
 			}
 		}
-		else if (choice == 5) {
+		else if (choice == 5) { //12 months of percentage for specified year
 			cout << "Insert game title:" << endl;
 			string temp3;
 			getline(cin, temp3);
@@ -579,12 +588,17 @@ int main()
 				if (validYearInput(temp) == true) {
 					auto start = chrono::steady_clock::now();
 					for (int i = 1; i < 13; i++) {
-						Game temp2 = jumpSearch(gameMap, temp3, i, temp);
-						if (temp2.getYear() == -1) {
+						if (temp == 2021 && i > 2) {
 							cout << i << ": No Info For This Month" << endl;
 						}
 						else {
-							cout << i << ": " << temp2.getAvgPeak() << endl;
+							Game temp2 = jumpSearch(gameMap, temp3, i, temp);
+							if (temp2.getYear() == -1) {
+								cout << i << ": No Info For This Month" << endl;
+							}
+							else {
+								cout << i << ": " << temp2.getAvgPeak() << endl; //same as 4 but output percentage rather than peak
+							}
 						}
 					}
 					auto end = chrono::steady_clock::now();
@@ -594,7 +608,7 @@ int main()
 					for (int i = 1; i < 13; i++) {
 						Game temp2 = FibonacciSearch(gameMap, temp3, i, temp);
 						if (temp2.getYear() == -1) {
-							cout << "";
+							cout << ""; //keep output statements empty to keep similar time constraints as jump search
 						}
 						else {
 							cout << "";
@@ -608,8 +622,8 @@ int main()
 				}
 			}
 		}
-		else if (choice == 6) {
-			cout << "Should duplicates be allowed? Y/N" << endl;
+		else if (choice == 6) { //all time highest player base games
+			cout << "Should duplicates be allowed? Y/N" << endl; //PUBG has 9 slots in leaderboard normally
 			string temp;
 			getline(cin, temp);
 			if (temp != "Y" && temp != "N") {
@@ -632,10 +646,10 @@ int main()
 				cout << endl;
 			}
 		}
-		else if (choice == 7) {
+		else if (choice == 7) { //exit while loop
 			cout << "\nThanks for your time!" << endl;
 		}
-		else {
+		else { //prevents incorrect choice inputs
 			cout << "Not a valid choice input!" << endl;
 		}
 	}
